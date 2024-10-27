a80323501050fabaa1c04f68b823de6ac708a0d3
Serhii Senkiv <senkiv34@gmail.com>
2024-10-27 15:51:47 +0100

    solution to jv-pecs

--- src/main/java/core/mate/academy/model/Bulldozer.java
+++ src/main/java/core/mate/academy/model/Bulldozer.java
@@ -5,11 +5,21 @@ package core.mate.academy.model;
  * Do not remove no-args constructor
  */
 public class Bulldozer extends Machine {
+    private String buldozerField;
+
     public Bulldozer() {
     }
 
     @Override
     public void doWork() {
         System.out.println("Bulldozer started to work");
+    }
+
+    public String getBuldozerField() {
+        return buldozerField;
+    }
+
+    public void setBuldozerField(String buldozerField) {
+        this.buldozerField = buldozerField;
     }
 }
--- src/main/java/core/mate/academy/model/Truck.java
+++ src/main/java/core/mate/academy/model/Truck.java
@@ -5,11 +5,21 @@ package core.mate.academy.model;
  * Do not remove no-field constructor
  */
 public class Truck extends Machine {
+    private String truckField;
+
     public Truck() {
     }
 
     @Override
     public void doWork() {
         System.out.println("Truck started to work");
+    }
+
+    public String getTruckField() {
+        return truckField;
+    }
+
+    public void setTruckField(String truckField) {
+        this.truckField = truckField;
     }
 }
